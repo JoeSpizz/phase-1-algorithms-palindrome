0ac46758d49d4fe1da79afcedaaabf0e1d2286fe
Joe <spizjo01@gmail.com>
2022-07-07 08:39:11 -0600

    commit tree 7b28dcfd1a46710b26a5a0ee36a1c79343e8ccdb

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function isPalindrome(word) {
+  let splitWord = word.split('')
+  let revWordArray = splitWord.reverse()
+  let invertedWord = revWordArray.join('')
+  if (word === invertedWord){
+    return true
+  }
+  else { return false}
   // Write your algorithm here
 }
 
 /* 
   Add your pseudocode here
+  // take a string. invert string; split each letter into array and
+  reverse array? 
+      if inverted string === string then return true
+      else return false
 */
 
 /*
@@ -13,6 +24,8 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
